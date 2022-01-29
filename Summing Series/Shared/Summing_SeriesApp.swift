76b9f8802d3b72c7bde2178f8f6da566b2ff1b92
Daksh <dpatel132@hawk.iit.edu>
2022-01-29 14:39:53 -0600

    a

---
+++ Summing Series/Shared/Summing_SeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Summing_SeriesApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Summing_SeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
